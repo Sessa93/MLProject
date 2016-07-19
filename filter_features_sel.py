eb31c2b98ef167b1ff077da8dcdbcdeeb4e6a8cc
Sessa93 <andrea.sessa@mail.polimi.it>
2016-07-19 10:17:59 +0200

    Started ANOVA

--- filter_features_sel.py
+++ filter_features_sel.py
@@ -3,29 +3,57 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from astroML.datasets import sdss_corrected_spectra
 import scipy.io as sio
-import pylab as pl
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_classif
+from sklearn import svm, datasets, feature_selection, cross_validation
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
 
-def selectF(X,Y):
-    #Load the data
-    #data = sdss_corrected_spectra.fetch_sdss_corrected_spectra()
-    #X = sdss_corrected_spectra.reconstruct_spectra(data)
-    #Y = data['lineindex_cln']
+def main():
 
-    X_indices = np.arange(X.shape[-1])
+    #Load the dataset from Matlab
+    data = sio.loadmat('baseline.mat')
+    train_x = (np.array(data['train_x']))
+    train_t = np.array(data['train_t'])
+    test_x = np.array(data['test_x'])
+    test_t = np.array(data['test_t'])
 
-    selector = SelectPercentile(f_classif, percentile=10)
-    selector.fit(X, Y)
+    X_indices = np.arange(train_x.shape[-1])
 
-    #print(selector.pvalues_)
+    #Plotting
+    X_f_scores, X_f_pval = f_classif(train_x, train_t.ravel())
+    fig, ax = plt.subplots(figsize=(8,6))
+    ax.plot(X_f_scores)
+    ax.set_title('Univariate Feature Selection: Classification F-Score')
+    ax.set_xlabel('features')
+    ax.set_ylabel('F-score')
+    plt.show()
 
-    #scores = -np.log10(selector.pvalues_)
-    #scores /= scores.max()
+    #SVM Fitting
 
-    #plt.bar(X_indices - .45, scores, width=.2, label=r'Univariate score ($-Log(p_{value})$)', color='g')
-    #plt.show()
+    tuned_parameters = [{'degree': ['3'], 'kernel': ['poly'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]}]
+    ###############################################################################
+    # Plot the cross-validation score as a function of percentile of features
+    score_means = list()
+    score_stds = list()
+    percentiles = (1, 3, 6, 10, 15, 20, 30, 40, 60, 80, 100)
 
-    return selector.transform(X).shape
+    for p in percentiles:
+        #Find best C, gamma
+        grid = GridSearchCV(SVC(), tuned_parameters, cv=5, scoring='%s' % p)
+        grid.fit(train_x, train_t)
+
+        print("Best parameters set found on development set:")
+        print()
+        print(grid.best_params_)
+
+        anova = feature_selection.SelectPercentile(feature_selection.f_classif, percentile=p)
+        # Compute cross-validation score using all CPUs
+        this_scores = cross_validation.cross_val_score(grid, train_x, train_t, n_jobs=1)
+        
+        score_means.append(this_scores.mean())
+        score_stds.append(this_scores.std())
+
+if __name__ == "__main__":
+    main()
