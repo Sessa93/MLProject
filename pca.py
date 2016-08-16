223ab5f7330a7f94b3c3226c7fcd410412773df2
Andrea <andrea.sessa@mail.polimi.it>
2016-08-16 14:48:38 +0200

    Completed PCA

--- pca.py
+++ pca.py
@@ -1,5 +1,5 @@
 #Author Andrea Sessa
-#Perform a filter feature selection
+#Perform a PCA analysis
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,9 +10,9 @@ from sklearn import svm, datasets, feature_selection, cross_validation
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.decomposition import PCA
 
 from sklearn import datasets
-
 
 def main():
     #Load the dataset from Matlab
@@ -25,37 +25,28 @@ def main():
     test_t = np.array(data['test_t']).reshape(800)
     X_indices = np.arange(train_x.shape[-1])
 
-    #Plotting
-    X_f_scores, X_f_pval = f_classif(train_x, train_t.ravel())
-    fig, ax = plt.subplots(figsize=(8,6))
-    ax.plot(X_f_scores)
-    ax.set_title('Univariate Feature Selection: Classification F-Score')
-    ax.set_xlabel('features')
-    ax.set_ylabel('F-score')
-    #plt.show()
-
     #SVM Fitting
     C = [-10,0,5]
     G = [-10,0,10]
     CF = [-10,0,5,10]
 
     # Plot the cross-validation score as a function of percentile of features
-    percentiles = (1, 10, 15, 20, 30, 40, 100)
+    components = (5,10,20,50,100,200)
     scores = list()
-    anovas = list()
     svcs = list()
+    pcas = list()
 
-    for p in percentiles:
+    for cc in components:
         best_c = 0
         best_g = 0
         best_cf = 0
-        best_anova = None
+        best_pca = None
         max_score = -np.inf
 
-        anova = feature_selection.SelectPercentile(f_classif, percentile=p)
-        anova.fit(train_x,train_t)
+        pca = PCA(n_components=cc)
+        pca.fit(train_x)
+        train = pca.transform(train_x)
 
-        train = anova.transform(train_x)
         for c in C:
             for g in G:
                 for cf in CF:
@@ -64,38 +55,32 @@ def main():
                     this_scores = cross_validation.cross_val_score(svc, train, train_t, n_jobs=-1, cv=5, scoring='accuracy')
                     mean_score = sum(this_scores)/len(this_scores)
 
-                    print("C: "+str(c)+" G: "+str(g)+" P: "+str(p)+" A: "+str(mean_score) + " CF: " +str(cf))
+                    print("C: "+str(c)+" G: "+str(g)+" CMPS: "+str(cc)+" A: "+str(mean_score) + " CF: " +str(cf))
                     if mean_score > max_score:
                         max_score = mean_score
                         best_svm = svc
-                        best_anova = anova
+                        best_pca = pca
         svcs.append(best_svm)
-        anovas.append(best_anova)
+        pcas.append(best_pca)
         scores.append(max_score)
 
     m_ind =  scores.index(max(scores))
     best_s = svcs[m_ind]
-    anova = anovas[m_ind]
-
-    print(scores)
+    pca = pcas[m_ind]
 
     # Test final model
-    test = anova.transform(test_x)
-    train = anova.transform(train_x)
+    test = pca.transform(test_x)
+    train = pca.transform(train_x)
     best_s.fit(train,train_t)
-    
     final_score = best_s.score(test,test_t)
     print(best_s)
     print("Final Accuracy: "+str(final_score))
 
-    pred_t = best_s.predict(test)
-    sio.savemat('predicted_anova.mat',dict(x=range(800),pred_t=pred))
-
     # Plot the result
     fig, ax = plt.subplots(figsize=(8,6))
-    ax.plot(percentiles, scores)
-    ax.set_title('Univariate Feature Selection: Classification F-Score')
-    ax.set_xlabel('Percentile')
+    ax.plot(components, scores)
+    ax.set_title('PCA Analysis')
+    ax.set_xlabel('N. Components')
     ax.set_ylabel('Accuracy')
     plt.show()
 
